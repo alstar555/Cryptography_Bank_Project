5ef99a36e30ec739aa3e3cfafe8b131b46436d54
Chris Reed <55092742+cjreed121@users.noreply.github.com>
2024-03-13 16:05:00 -0400

    Remove namespace std from SHA-1

--- SHA-1.cpp
+++ SHA-1.cpp
@@ -1,12 +1,10 @@
 #include <string>
 #include <cstdint>
 
-using namespace std;
-
 class SHA1 {
 public:
     std::string operator()(const std::string& message) {
-        string msg_hashed = message;
+        std::string msg_hashed = message;
         return msg_hashed;
 
     }

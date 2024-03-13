4178d8df0316ca32555fb50f15398c2082b6a12f
Angelica <alstar555@gmail.com>
2024-03-13 15:50:31 -0400

    SHA-1 temp file

---
+++ SHA-1.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <cstdint>
+
+using namespace std;
+
+class SHA1 {
+public:
+    std::string operator()(const std::string& message) {
+        string msg_hashed = message;
+        return msg_hashed;
+
+    }
+
+private:
+  
+};
